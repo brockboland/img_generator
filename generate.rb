578f525a991b5b9034959a73b74268dc5c78b35d
Brock Boland <brock@brockboland.com>
2013-02-04 20:42:20 -0700

    Manually color each pixel instead of using rect: it's a bit quicker

--- generate.rb
+++ generate.rb
@@ -2,13 +2,13 @@ begin_time= Time.now
 require 'chunky_png'
 require 'fileutils'
 
-# How many columns of "pixels"?
+# How many columns of blocks?
 width = 3
 
-# How many rows of "pixels"?
+# How many rows of blocks?
 height = 3
 
-# How many pixels should each "pixel" actually be?
+# How many pixels should each block actually be?
 scale = 40
 
 # Image file output directory
@@ -22,12 +22,17 @@ FileUtils.mkdir(dir)
 bits = width * height
 range = 2**(width * height)
 
+# Loop over each variation
 (0..range - 1).each do |i|
+  # Convert the number to binary and zero-pad
+  # 0 will become white block, 1 will become black block
   binary = i.to_s(2)
   binary = '0' * (bits - binary.length) + binary
-  
+
+  # Create a blank white canvas
   png = ChunkyPNG::Image.new(width * scale, height * scale, ChunkyPNG::Color::WHITE)
 
+  # Loop over each bit in the string, each of which represents a block of color
   (0..height-1).each do |row|
     (0..width-1).each do |column|
       index = (row * width) + column
@@ -35,13 +40,23 @@ range = 2**(width * height)
       # Image starts out with a white background, so only set the pixels that
       # should be black
       if binary[index] == '1'
-        # png[column, row] = ChunkyPNG::Color::BLACK
-        x = column * scale
-        y = row * scale
-        png.rect(x, y, x + scale, y + scale, ChunkyPNG::Color::BLACK, ChunkyPNG::Color::BLACK)
+        # Determine the boundaries of this block of color
+        startX = column * scale
+        endX = startX + scale - 1
+        startY = row * scale
+        endY = startY + scale - 1
+
+        # Color in the block, one pixel at a time
+        (startX..endX).each do |x|
+          (startY..endY).each do |y|
+            png[x, y] = ChunkyPNG::Color::BLACK
+          end
+        end
       end
     end
   end
+
+  # Save the resulting image
   png.save('output/' + i.to_s + '.png')
 end
 
