16c341f4c14395a7ad8ddcdf1b1638aeec5ad6a9
Brock Boland <brock@brockboland.com>
2013-02-04 20:32:31 -0700

    Use rectangles to draw larger images by scaling up. Slows things down

--- generate.rb
+++ generate.rb
@@ -2,10 +2,17 @@ begin_time= Time.now
 require 'chunky_png'
 require 'fileutils'
 
-width = 4
-height = 4
-dir = 'output'
+# How many columns of "pixels"?
+width = 3
 
+# How many rows of "pixels"?
+height = 3
+
+# How many pixels should each "pixel" actually be?
+scale = 40
+
+# Image file output directory
+dir = 'output'
 
 #########
 # Make sure we've got an empty output directory
@@ -19,7 +26,7 @@ range = 2**(width * height)
   binary = i.to_s(2)
   binary = '0' * (bits - binary.length) + binary
   
-  png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::WHITE)
+  png = ChunkyPNG::Image.new(width * scale, height * scale, ChunkyPNG::Color::WHITE)
 
   (0..height-1).each do |row|
     (0..width-1).each do |column|
@@ -28,7 +35,10 @@ range = 2**(width * height)
       # Image starts out with a white background, so only set the pixels that
       # should be black
       if binary[index] == '1'
-        png[column, row] = ChunkyPNG::Color::BLACK
+        # png[column, row] = ChunkyPNG::Color::BLACK
+        x = column * scale
+        y = row * scale
+        png.rect(x, y, x + scale, y + scale, ChunkyPNG::Color::BLACK, ChunkyPNG::Color::BLACK)
       end
     end
   end
