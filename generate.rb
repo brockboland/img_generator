3d3565cca2db5784a04e600f8aac7a211351409c
Brock Boland <brock@brockboland.com>
2013-02-04 20:24:45 -0700

    Improved runtime by starting with a white PNG and only setting pixels that should be black

--- generate.rb
+++ generate.rb
@@ -1,8 +1,9 @@
+begin_time= Time.now
 require 'chunky_png'
 require 'fileutils'
 
-width = 3
-height = 3
+width = 4
+height = 4
 dir = 'output'
 
 
@@ -18,18 +19,20 @@ range = 2**(width * height)
   binary = i.to_s(2)
   binary = '0' * (bits - binary.length) + binary
   
-  png = ChunkyPNG::Image.new(width, height)
+  png = ChunkyPNG::Image.new(width, height, ChunkyPNG::Color::WHITE)
 
   (0..height-1).each do |row|
     (0..width-1).each do |column|
       index = (row * width) + column
 
-      if binary[index] == '0'
-        png[column, row] = ChunkyPNG::Color('white')
-      else
-        png[column, row] = ChunkyPNG::Color('black')
+      # Image starts out with a white background, so only set the pixels that
+      # should be black
+      if binary[index] == '1'
+        png[column, row] = ChunkyPNG::Color::BLACK
       end
     end
   end
   png.save('output/' + i.to_s + '.png')
 end
+
+puts 'Run time: ' + (Time.now - begin_time).to_s + ' seconds'
