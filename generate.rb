166c2935558e6b4c88fdf9fdd0cbcdabc0673869
Brock Boland <brock@brockboland.com>
2013-02-04 20:16:22 -0700

    Output the generated image as a PNG

--- generate.rb
+++ generate.rb
@@ -1,5 +1,15 @@
-width = 4
-height = 4
+require 'chunky_png'
+require 'fileutils'
+
+width = 3
+height = 3
+dir = 'output'
+
+
+#########
+# Make sure we've got an empty output directory
+FileUtils.rm_rf(dir)
+FileUtils.mkdir(dir)
 
 bits = width * height
 range = 2**(width * height)
@@ -7,13 +17,19 @@ range = 2**(width * height)
 (0..range - 1).each do |i|
   binary = i.to_s(2)
   binary = '0' * (bits - binary.length) + binary
-  disp = binary.gsub('1', 'X').gsub('0', ' ')
   
+  png = ChunkyPNG::Image.new(width, height)
+
   (0..height-1).each do |row|
-    start = row * width
-    finish = start + width - 1
-    puts disp[start..finish]
+    (0..width-1).each do |column|
+      index = (row * width) + column
+
+      if binary[index] == '0'
+        png[column, row] = ChunkyPNG::Color('white')
+      else
+        png[column, row] = ChunkyPNG::Color('black')
+      end
+    end
   end
-  
-  puts "\n\n"
+  png.save('output/' + i.to_s + '.png')
 end
